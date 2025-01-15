e0b8d963ddecd0485b82b8473cb3dc085fbe7e12
shashwatsrv <shashwat789saurav@gmail.com>
2025-01-15 17:53:07 +0530

    Add files via upload

---
+++ 2_variables.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    int a = 3;
+    float b = 7.5;
+    char c = 'c';
+    int d=8;
+    int e=3+8;
+    printf("The value of a is %d\n", a); // for integers
+    printf("The value of b is %f\n", b); // for float
+    printf("The value of c is %c\n", c); // for characters
+    printf("Sum of a and d is %d\n", a+d); // for sum of integers
+    printf("Sum of a and d is %d\n", e); // for sum of integers
+
+    return 0;
+}
